16ea5d955485c17bb39f94988a7d36af303b36ac
SorinTautu1991 <sorin.tautu@gmail.com>
2021-05-22 11:24:24 +0300

    Hooks

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
@@ -13,7 +12,6 @@ import {
   OptionsContainer,
   LogoContainer,
   OptionLink,
-  OptionDiv,
 } from './header.styles';
 
 const Header = ({ currentUser, hidden, signOutStart }) => (
